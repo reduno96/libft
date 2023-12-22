9e8aa76c64eaace713f7e4233c27f2927539c12e
reduno96 <reduno96@gmail.com>
2023-12-22 15:44:35 +0100

    Refactor code to improve performance and readability

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Red-Mor <reduno96@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 19:46:11 by rel-mora          #+#    #+#             */
-/*   Updated: 2023/12/21 22:57:27 by rel-mora         ###   ########.fr       */
+/*   Updated: 2023/12/22 15:41:11 by Red-Mor          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "libft.h"
 
@@ -19,16 +19,15 @@ int ft_iset(char const  *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] != c)
-			i++;
-		else
+		if (s[i] == c)
 			return (1);
+		i++;
 	}
 	return (0);
 
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)\
 {
 	int	i;
 	int	j;
@@ -37,29 +36,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	if (!s1 || !set)
 		return (NULL);
-	while (s1[i]&& ft_iset(s1, set[i]))
+	while (s1[i]&& ft_iset(set, s1[i]))
 		i++;
-	j = ft_strlen(s1);
-	while (j > i && ft_iset(s1, set[j]))
+	j = ft_strlen(s1) - 1;
+	while (j > i && ft_iset(set, s1[j]))
 		j--;
-	k = j - i;
+	k = j- i + 1;
 	return (ft_substr(s1, i, k));
 }
-/* char *ft_strtrim(char const *s1, char const *set)
-{
-    size_t start, end, len;
-
-    if (!s1 || !set)
-        return NULL;
-
-    start = 0;
-    while (s1[start] && strchr(set, s1[start]))
-        start++;
-
-    end = strlen(s1);
-    while (end > start && strchr(set, s1[end - 1]))
-        end--;
-
-    len = end - start;
-    return strndup(s1 + start, len);
-} */
