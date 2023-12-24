b64d450e222a1effcfd24a70c89a2049d2064d85
reduno96 <reduno96@gmail.com>
2023-12-24 16:50:12 +0100

    Add ft_putnbr_fd function to handle printing integers to a file descriptor

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+    long long_number;
+
+    long_number = n;
+    if (long_number < 0)
+    {    ft_putchar_fd('-', fd);
+        long_number = -long_number;
+        }
+    if (long_number >= 10)
+    {
+        ft_putnbr_fd(long_number / 10, fd);
+        ft_putnbr_fd(long_number % 10, fd);
+    }
+    if (long_number <= 9)
+        ft_putchar_fd((long_number + '0'), fd);
+}
