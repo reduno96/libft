680fff5ee42da395d1824aa029dc6f48fe597882
reduno96 <reduno96@gmail.com>
2024-01-05 21:22:17 +0100

    modification

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 20:51:21 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/01/04 21:38:45 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/01/05 20:24:54 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	hs = (char *) haystack;
 	nd = (char *) needle;
 	i = 0;
+	if (nd[i] == '\0')
+		return (hs);
 	if (len <= 0 || hs[i] == '\0')
 		return (NULL);
 	length_needle = ft_strlen(nd);
-	if (nd[i] == '\0')
-		return (hs);
 	while (hs[i] && (i + length_needle) <= len)
 	{
 		if (ft_strncmp((hs + i), nd, length_needle) == 0)
